e5c623c61e548ab02c6ea4429ad9df211c2a71aa
Keiran Sweet <Keiran@gmail.com>
2017-07-01 03:03:52 +0000

    Updates

--- scripts/bootstrapcompilemaster.sh
+++ scripts/bootstrapcompilemaster.sh
@@ -12,19 +12,19 @@ while getopts :p:c:a:e: opt "$@"; do
   case $opt in
     p)
       #echo "-p was triggered, Parameter: $OPTARG" >&2
-      echo "$OPTARG" > /etc/puppetlabs/puppet/ssl/privkey.pem
+      /bin/echo "$OPTARG" > /etc/puppetlabs/puppet/ssl/privkey.pem
       ;;
     c)
       #echo "-c was triggered, Parameter: $OPTARG" >&2
-      echo "$OPTARG" > /etc/puppetlabs/puppet/ssl/cert.pem
+      /bin/echo "$OPTARG" > /etc/puppetlabs/puppet/ssl/cert.pem
       ;;
     a)
       #echo "-a was triggered, Parameter: $OPTARG" >&2
-      echo "$OPTARG" > /etc/puppetlabs/puppet/ssl/ca.pem
+      /bin/echo "$OPTARG" > /etc/puppetlabs/puppet/ssl/ca.pem
       ;;
     e)
       #echo "-e was triggered, Parameter: $OPTARG" >&2
-      echo "$OPTARG" > /etc/puppetlabs/puppet/ssl/eyaml.pem
+      /bin/echo "$OPTARG" > /etc/puppetlabs/puppet/ssl/eyaml.pem
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
