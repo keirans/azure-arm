4af825210394b65b976d22a729421d8b3972231d
Keiran Sweet <Keiran@gmail.com>
2017-07-01 01:46:58 +0000

    Adding poc bootstrap

---
+++ scripts/bootstrapcompilemaster.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# p - instance private key
+# c - instance cert
+# a - Puppet CA public cert
+# e - eyaml private key
+#
+
+mkdir -p /etc/puppetlabs/puppet/ssl/
+
+while getopts :p:c:a:e: opt "$@"; do
+  case $opt in
+    p)
+      #echo "-p was triggered, Parameter: $OPTARG" >&2
+      echo $OPTARG > /etc/puppetlabs/puppet/ssl/privkey.pem
+      ;;
+    c)
+      #echo "-c was triggered, Parameter: $OPTARG" >&2
+      echo $OPTARG > /etc/puppetlabs/puppet/ssl/cert.pem
+      ;;
+    a)
+      #echo "-a was triggered, Parameter: $OPTARG" >&2
+      echo $OPTARG > /etc/puppetlabs/puppet/ssl/ca.pem
+      ;;
+    e)
+      #echo "-e was triggered, Parameter: $OPTARG" >&2
+      echo $OPTARG > /etc/puppetlabs/puppet/ssl/eyaml.pem
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+
+done
