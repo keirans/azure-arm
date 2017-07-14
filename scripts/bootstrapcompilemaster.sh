a2d0521adbd5c53923fb5a89ee04c6ea4b93f183
Keiran Sweet <Keiran@gmail.com>
2017-07-14 07:51:31 +0000

    Cleaning up templates and adding misc scripts

--- scripts/bootstrapcompilemaster.sh
+++ scripts/bootstrapcompilemaster.sh
@@ -82,11 +82,18 @@ az keyvault secret download --name ${COMPILEMASTERFQDNVAULT}-cert --vault-name p
 rm -f /bin/python
 ln -s /bin/python2 /bin/python
 
-# Bootstrap from the master
+# Bootstrap from the master - Note this goes into the background.
 curl -k https://puppetmaster.example.com:8140/packages/current/install.bash | sudo bash -s main:dns_alt_names='puppetmaster.example.com,puppet.example.com,puppet,puppetmaster,`hostname`,`hostname`.example.com'
 
-# Chown the Files in the eyaml dir
-chown -R pe-puppet:pe-puppet /etc/puppetlabs/puppet/eyaml/
-
-
-
+# Chown the Files in the eyaml dir as soon as the user account is available
+# This loop should have a timeout, to be added.
+while true
+do
+  getent passwd pe-puppet > /dev/null
+    if [ $? -eq 0 ]; then
+      chown -R pe-puppet:pe-puppet /etc/puppetlabs/puppet/eyaml/
+      exit 0
+  else
+    sleep 30
+  fi
+done
