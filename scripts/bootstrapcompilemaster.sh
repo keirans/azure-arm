a3c63dfe62c93cb641fc6fcb21f7e8a99cb79119
Keiran Sweet <Keiran@gmail.com>
2017-07-19 06:34:19 +0000

    Updating scripts and documentation

--- scripts/bootstrapcompilemaster.sh
+++ scripts/bootstrapcompilemaster.sh
@@ -1,16 +1,52 @@
 #!/bin/bash
+# Bootstrap compilemaster - Install and configure Puppet Compile master automatically
+# Keiran Sweet <Keiran@gmail.com>
+# ------------------------------------------------------------------------------------------
+# Example code - Use at your own risk
+# 
+# This script, when used in conjuntion with the Azure custom script extension, Azure Keyvault and Puppet enterprise
+# enabled you to install 
+#
+# Obviously this script turns off a number of security controls that you would not do outside of PoC environments.
+#
+#
+# This script takes the following actions
+# - Takes 3 mandatory arguements during launch to be passed to the Azure CLI for authentication
+#   - Tennant ID
+#   - Username
+#   - Password
+# - Disable iptables
+# - Add a Puppetmaster entry to hosts in the place of functioning DNS in my PoC environment
+# - Install a set of required software packages such as git, python, pip and the Azure CLI
+# - Login to the Azure API using the Azure CLI using the passed service principals, granting the
+#   instance access to the puppetsecrets keyvault that contains all the compile master pre-generated
+#   secrets.
+# - Login to the Azure API using the CLI tooling
+# - Based on the hostname of the instance, fetch all the required node specific secrets from the
+#   key vault and place them on the filesystem alongside all the eyaml files if present.
+# - Logout of the Azure account as once the files are put in place, access is no longer required
+# - Bootstrap the Puppet installation from the master of masters, there should be classifier rules in 
+#   place that will ensure this node will be classified as puppet master. Because we are using pre-created
+#   and signed keys, we will be given a catalogue.
+# - The script will then go into a loop for up to 10 minutes to ensure that all the configuration is in place
+#   in a timely fashion. This obviously can be optimised.
+# - The result is a fully functioning compile master.
+#
 
+# Usage
 if [ $# -eq 0 ]; then
   echo "Compile Master Bootstrap"
   echo "Usage: $0 -t <tennantid> -u <username> -p <password>"
   exit 1
 fi
 
+# Define our environment settings.
 export COMPILEMASTERPREFIX='compilemaster'
 export FQDN='.example.com'
 export  COMPILEMASTERFQDNVAULT=`echo $(hostname)${FQDN} | sed 's/\./-/g'`
 
 # Put in the Puppet master IPs in the place of having DNS for now.
+# 
 echo "=> Setting up host entries for the instance"
 echo "$(hostname -I) $(hostname)${FQDN} $(hostname)" >> /etc/hosts
 echo "10.1.0.4 puppetmaster.example.com puppetmaster" >> /etc/hosts
@@ -89,7 +125,7 @@ az keyvault secret download --name ${COMPILEMASTERFQDNVAULT}-cert --vault-name p
 
 # Logout of the azure environment - We no login need any access
 echo "=> Logging out of the Azure API - We no longer need these privs"
-#az logout
+az logout
 
 # Fix python again
 echo "=> Returning the System Python version to version 2 - We are done with the Azure CLI"
@@ -104,9 +140,9 @@ chmod 755 /var/tmp/install.bash
 echo "=> Completed execution of Puppet Installation commands from the Master of Masters"
 
 #
-# Lets wait for up to 6 minutes for the initial Puppet run to complete before running Puppet
+# Lets wait for up to 10  minutes for the initial Puppet run to complete before running Puppet
 # manually 2 more times to ensure everything is online, green in the console and the permissions
-# on the eyaml files are correct.
+# on the eyaml files are correct. This too can be optimised.
 #
 echo "=> Entering a wait period of up to 10 minutes for the deployment to become ready"
 COUNT=0
--- scripts/generate_certs.sh
+++ scripts/generate_certs.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
-#set -x
-# This script does the following actions
-# Creates a resource group
-# Creates a keyvault
-# Generates a set of CompileMaster Certs with altdns names
+# Generate Certs - Pregenerate and store Puppet compile master certs into Azure Key Vault
+# Keiran Sweet <Keiran@gmail.com>
+# ------------------------------------------------------------------------------------------
+# Example code - Use at your own risk
 #
+# This script demonstrates how to do the following when run from a Puppet Master of Masters
+# with the Azure CLI configured and authenticated with the correct access to create Resource Groups
+# Key Vaults and other resources.
 #
+# - Create a resource group for the Keyvault
+# - Create a keyvault to store the compile master secrets and related data (puppetsecretsvault)
+# - Create a set of compileMaster Certs with altdns names (40 certs by default)
+# - Upload the keys and certs to the keyvault that we have just created
+# - Upload the eyaml private key to the keyvault that we have just created
+# 
+# The structure of the key vault content, using compilemaster0 as an example is below:
 #
-
-export RGNAME='keyvaultsg'
-export VAULTNAME='puppetvault'
+# ->  puppetsecretsvault
+#   -> secrets
+#     -> compilemaster0-example-com-cert
+#     -> compilemaster0-example-com-privkey
+#     -> compilemaster0-example-com-pubkey 
+#     -> puppetmaster-CA-cert
+#     -> eyamlprivate
+#
+# Modify these values for your environment.
+#
+export RGNAME='puppetsecretsrg'
+export VAULTNAME='puppetsecretsvault'
 export COMPILEMASTERPREFIX='compilemaster'
-export FQDN='.domain.com'
-export DNSALTNAMES=''
+export FQDN='.example.com'
 export COMPILEMASTERCOUNT='40'
 export AZUREREGION='australiasoutheast'
-export EYAMLPRIVATEKEYPATH=''
+export EYAMLPRIVATEKEYPATH='/etc/puppetlabs/puppet/eyaml/private_key.pkcs7.pem'
 
+# Backup your SSL data first.
+echo "Backing up all SSL data into /var/tmp/ first"
+tar -zcvf /var/tmp/puppet-ssl-backup.tgz.$$ /etc/pupppetlabs/puppet/ssl/
+chmod 700 /var/tmp/puppet-ssl-backup.tgz.$$
+
+# Create your Resource Group and Key Vault
 echo "Creating Resource Group ${RGNAME} in ${AZUREREGION}"
 az group create -n ${RGNAME} -l ${AZUREREGION} > /dev/null
 echo "Creating keyvault ${VAULTNAME} in ${RGNAME}"
 az keyvault create --enabled-for-deployment true --location ${AZUREREGION} --resource-group ${RGNAME} --enabled-for-template-deployment true --sku standard -n ${VAULTNAME} > /dev/null
 
-
-
-# Lets Create a bunch of suitable compile master keys
+# Lets create a set of suitable compile master keys based on the count variable
 seq 0 ${COMPILEMASTERCOUNT} | while read COMPILEMASTERID
 do
   echo "Generating Compile Master certs for ${COMPILEMASTERPREFIX}${COMPILEMASTERID}${FQDN}"
@@ -37,5 +58,12 @@ do
 az keyvault secret set --file /etc/puppetlabs/puppet/ssl/public_keys/${COMPILEMASTERPREFIX}${COMPILEMASTERID}${FQDN}.pem --description "${COMPILEMASTERPREFIX}${COMPILEMASTERID}${FQDN}pubkey" --vault-name ${VAULTNAME} --name ${COMPILEMASTERFQDNVAULT}-pubkey --encoding ascii > /dev/null
   echo "Uploading Public cert ${COMPILEMASTERPREFIX}${COMPILEMASTERID}${FQDN} to ${VAULTNAME} as ${COMPILEMASTERFQDNVAULT}-cert" 
   az keyvault secret set --file /etc/puppetlabs/puppet/ssl/certs/${COMPILEMASTERPREFIX}${COMPILEMASTERID}${FQDN}.pem --description "${COMPILEMASTERPREFIX}${COMPILEMASTERID}${FQDN}-cert" --vault-name ${VAULTNAME} --name ${COMPILEMASTERFQDNVAULT}-cert --encoding ascii > /dev/null
-
 done
+
+# If the eyaml key exists, lets also upload that as 'eyamlprivate' into the vault
+
+if [ -f $EYAMLPRIVATEKEYPATH ]; then
+  echo "Found eyaml private key. Uploading to vault as eyamlprivate"
+  az keyvault secret set --file $EYAMLPRIVATEKEYPATH --description "hiera eyaml private key" --vault-name ${VAULTNAME} --name eyamlprivate --encoding ascii > /dev/null
+fi
+
